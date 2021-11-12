05d187a349d5dab1e23571f94f40d7b33cac9fc4
Rakibul8001 <ghumpori8001@gmail.com>
2021-11-12 14:20:06 +0600

    user delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function AauthAcessToken(){
-        return $this->hasMany('\App\Models\OauthAccessToken');
+    public function jobs(){
+        return $this->hasMany('\App\Models\Job');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,12 @@ Route::post('/register',[UserController::class, 'register']);
 Route::post('/login',[UserController::class, 'login']);
 Route::post('/logout/user/{id}',[UserController::class, 'logout']);
 
-
 //protected routes
 Route::group(['middleware'=>['auth:sanctum']], function(){
     Route::get('/users',[UserController::class, 'index']);
     Route::post('/user/create',[UserController::class, 'createUser']);
     Route::put('/user/update/{id}',[UserController::class, 'updateUser']);
+    Route::delete('/user/{id}',[UserController::class, 'deleteUser']);
     Route::apiResource('jobs',JobController::class);
 });
 
