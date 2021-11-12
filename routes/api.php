a6af26317f77092cf5f8fe46c521b4783b9e7dbb
Rakibul8001 <ghumpori8001@gmail.com>
2021-11-12 15:34:10 +0600

    modify api.php

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,6 @@ Route::post('/logout/user/{id}',[UserController::class, 'logout']);
 
 //protected routes
 Route::group(['middleware'=>['auth:sanctum']], function(){
-    // Route::get('/users',[UserController::class, 'index']);
-    // Route::post('/user/create',[UserController::class, 'createUser']);
-    // Route::put('/user/update/{id}',[UserController::class, 'updateUser']);
-    // Route::delete('/user/{id}',[UserController::class, 'deleteUser']);
     Route::apiResource('users',UserController::class);
     Route::apiResource('jobs',JobController::class);
 });
